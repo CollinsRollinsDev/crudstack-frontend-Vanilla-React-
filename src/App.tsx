8328c179337f5510a35c95a3b3ed84f60e68a557
CollinsRollinsDev <collinsrollins07@gmail.com>
2022-02-21 12:27:25 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
+  try {
+    const test = async() => {
+     const res = await fetch(`http://localhost:8088/login?queryP=test`, {
+      method:"post",
+      body:JSON.stringify({
+        emailAddress:'kelvin',
+        password:'pass'
+      }),
+      headers:{
+        "Content-Type":"application/json"
+      },
+      credentials:'include'
+    })
+    const data = await res.json()
+    console.log(data)
+    }
+    test()
+   } catch (error) {
+     console.log(error)
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' />
+      </Routes>
+    </Router>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
