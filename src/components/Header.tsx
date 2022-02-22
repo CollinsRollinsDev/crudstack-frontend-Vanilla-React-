470616e8d8423401ac031ac98229e7a48390d48d
CollinsRollinsDev <collinsrollins07@gmail.com>
2022-02-22 15:57:15 +0100

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -34,7 +34,7 @@ const Header = ({ currentPage }: Incoming) => {
   };
 
   useLayoutEffect(() => {
-    // cookieDecode();
+    cookieDecode();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
